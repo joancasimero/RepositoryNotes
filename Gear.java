a570345331d871a511a1774e82d2cece0a0fa265
Joan Casimero <111473276+joancasimero@users.noreply.github.com>
2022-09-01 09:59:49 +0800

    Add files via upload

---
+++ Gear.java
@@ -0,0 +1,14 @@
+public class Gear {
+    
+    //Properties
+    String name;
+    int defense;
+    float weight;
+    String description;
+    String rarity;
+    boolean hasElementResistance;
+
+    public void showDescription() {
+        System.out.println("Description: " + description);
+    }
+}
